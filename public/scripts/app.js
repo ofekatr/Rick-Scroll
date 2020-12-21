f1a5cf1b1bf06546b34f7c283e2efc40c8812261
Ofek Atar <ofekatar98@gmail.com>
2020-12-21 16:39:10 +0200

    Finished video scroll scene.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,3 +5,26 @@ const text = intro.querySelector("h1");
 const section = document.querySelector("section");
 const end = section.querySelector("h1");
 
+const controller = new ScrollMagic.Controller();
+const scene = new ScrollMagic.Scene({
+  duration: 9000,
+  triggerElement: intro,
+  triggerHook: 0,
+})
+  .addIndicators()
+  .setPin(intro)
+  .addTo(controller);
+
+let accelamount = 0.1;
+let scrollPos = 0;
+let delay = 0;
+
+scene.on("update", (e) => {
+    scrollPos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+    delay += (scrollPos - delay) * accelamount;
+
+    video.currentTime = delay;
+}, 33.3);
