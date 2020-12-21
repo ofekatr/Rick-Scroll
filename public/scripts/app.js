30394caa6a947c2811dbf953970893acbd4fd253
Ofek Atar <ofekatar98@gmail.com>
2020-12-21 16:19:36 +0200

    Implemented HTML and CSS.

---
+++ public/scripts/app.js
@@ -0,0 +1,7 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
